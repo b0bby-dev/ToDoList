c21a64ac86c6835b546de03cd235dc72b538483f
B0bby-BK <bobbybk03@gmail.com>
2025-07-03 20:28:32 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,28 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, View } from 'react-native'
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
+const App = () => {
+  return (<View style={styles.container}></View>);
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-});
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+  }
+}
 
-export default App;
+)
+export default App; 
